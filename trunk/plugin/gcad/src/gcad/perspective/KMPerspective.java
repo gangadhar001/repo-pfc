1d7bbaf1143d666038a834cdb844661e7b0f440d
juanro.1987 <juanro.1987@b36858b7-eb87-cb19-e93d-fc6c9a9158f3>
2010-11-03 18:00:01 +0000

    - Creado el proyecto para el plug-in - Creada la perspectiva con dos vistas basicas

---
+++ trunk/plugin/gcad/src/gcad/perspective/KMPerspective.java
@@ -0,0 +1,28 @@
+package gcad.perspective;
+
+import org.eclipse.ui.IFolderLayout;
+import org.eclipse.ui.IPageLayout;
+import org.eclipse.ui.IPerspectiveFactory;
+
+public class KMPerspective implements IPerspectiveFactory {
+
+	@Override
+	public void createInitialLayout(IPageLayout layout) {
+		// Get the editor area identifier
+		String idEditorArea = layout.getEditorArea();
+		
+		// Put the "Outline" View on the left 
+		layout.addView(IPageLayout.ID_OUTLINE, IPageLayout.LEFT, 0.25f, idEditorArea);
+		
+		IFolderLayout folderBottom = layout.createFolder("bottom", IPageLayout.BOTTOM, 0.66f, idEditorArea);
+		
+		// Add standard "Problems" and "Task List" Views on the bottom
+		folderBottom.addPlaceholder(IPageLayout.ID_PROBLEM_VIEW);
+		folderBottom.addPlaceholder(IPageLayout.ID_TASK_LIST);
+		
+		// TODO: poner otras vistas
+		
+
+	}
+
+}
