5929d04867e519ee0351a6bf18f0ff830294d36a
juanro.1987 <juanro.1987@b36858b7-eb87-cb19-e93d-fc6c9a9158f3>
2010-11-25 12:04:14 +0000

    - Empezado a hacer el menu para insertar nuevo conocimiento (propuestas/respuestas). TODO: no se puede pinchar en el menu

--- plugin/gcad/src/gcad/perspective/KMPerspective.java
+++ plugin/gcad/src/gcad/perspective/KMPerspective.java
@@ -1,16 +1,12 @@
 package gcad.perspective;
 
-import gcad.wizards.DBConnectionWizard;
-
-import org.eclipse.jface.wizard.WizardDialog;
 import org.eclipse.ui.IFolderLayout;
 import org.eclipse.ui.IPageLayout;
 import org.eclipse.ui.IPerspectiveFactory;
-import org.eclipse.ui.PlatformUI;
 
 public class KMPerspective implements IPerspectiveFactory {
 	
-	private static final String PROPOSAL_VIEW_ID = "gcad.view.issues";
+	private static final String PROPOSAL_VIEW_ID = "gcad.category.view.proposal";
 		
 	@Override
 	public void createInitialLayout(IPageLayout layout) {
@@ -18,19 +14,22 @@ public class KMPerspective implements IPerspectiveFactory {
         String idEditorArea = layout.getEditorArea();
         
         // Put the "Outline" View on the left 
-        //layout.addView(IPageLayout.ID_OUTLINE, IPageLayout.LEFT, 0.25f, idEditorArea);
+        layout.addView(IPageLayout.ID_OUTLINE, IPageLayout.LEFT, 0.25f, idEditorArea);
         
         IFolderLayout folderBottom = layout.createFolder("bottom", IPageLayout.BOTTOM, 0.66f, idEditorArea);
         
         // Add standard "Problems" and "Task List" Views on the bottom
         folderBottom.addPlaceholder(IPageLayout.ID_PROBLEM_VIEW);
         folderBottom.addPlaceholder(IPageLayout.ID_TASK_LIST);
+        // Add "Proposals View" on the bottom, too
+        folderBottom.addPlaceholder(PROPOSAL_VIEW_ID);
+
         
         // TODO: poner otras vistas
         // Put the "Issues" view on the left, with the "Outline" view
-        IFolderLayout folderLeft = layout.createFolder("left", IPageLayout.LEFT, 0.20f, idEditorArea);
+        /*IFolderLayout folderLeft = layout.createFolder("left", IPageLayout.LEFT, 0.20f, idEditorArea);
         folderLeft.addView(PROPOSAL_VIEW_ID);
-        folderLeft.addView(IPageLayout.ID_OUTLINE);
+        folderLeft.addView(IPageLayout.ID_OUTLINE);*/
 		
 		// TODO: poner otras vistas
 		
