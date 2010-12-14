9f1d3d15973203dedd89c05cd8bd68ce9f4a27aa
juanro.1987 <juanro.1987@b36858b7-eb87-cb19-e93d-fc6c9a9158f3>
2010-12-14 16:52:14 +0000

    Creados sourceProvider para todos los menus TODO: crear los perfiles de XML para activar/desactivar menus

--- trunk/plugin/gcad/src/model/business/knowledge/Operations.java
+++ trunk/plugin/gcad/src/model/business/knowledge/Operations.java
@@ -2,5 +2,14 @@ package model.business.knowledge;
 
 public enum Operations {
 	// TODO: Operaciones del Gestor de Proyectos
-	CreateProject
+	CreateProject,
+	
+	//TODO: Operaciones del gestor de conocimiento (propuestas/respuestas)
+	AddProposal,
+	ModifyProposal,
+	DeleteProposal,
+	AddAnswer,
+	ModifyAnswer,
+	DeleteAnswer
+	
 }
