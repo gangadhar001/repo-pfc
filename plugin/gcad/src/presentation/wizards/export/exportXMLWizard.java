f3142d0f033a4c490bea75c1c3613ca87f3ecc95
juanro.1987 <juanro.1987@b36858b7-eb87-cb19-e93d-fc6c9a9158f3>
2011-02-06 13:51:36 +0000

    - Creada la funcionalidad para exportar la base de datos a un XML

--- plugin/gcad/src/presentation/wizards/export/exportXMLWizard.java
+++ plugin/gcad/src/presentation/wizards/export/exportXMLWizard.java
@@ -1,26 +1,105 @@
 package presentation.wizards.export;
 
+import internationalization.BundleInternationalization;
+
+import model.business.control.Controller;
+
+import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.jface.viewers.IStructuredSelection;
-import org.eclipse.jface.wizard.Wizard;
-import org.eclipse.ui.IExportWizard;
-import org.eclipse.ui.IWorkbench;
+import org.eclipse.jface.wizard.WizardPage;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Event;
+import org.eclipse.swt.widgets.FileDialog;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Listener;
+import org.eclipse.swt.widgets.Text;
 
-public class exportXMLWizard extends Wizard implements IExportWizard {
+public class exportXMLWizard extends WizardPage {
 
-	public exportXMLWizard() {
-		// TODO Auto-generated constructor stub
+	private FileDialog fileDialog;
+	private Text fileText;
+	private Button selectFileButton;
+	private String filePath = "";
+	
+	protected exportXMLWizard() {
+		super("nombrwe");
+		setTitle("title");
+		setDescription("a");
 	}
 
-	@Override
-	public void init(IWorkbench workbench, IStructuredSelection selection) {
-		// TODO Auto-generated method stub
-
+	
+	public void createControl(Composite parent) {
+		Composite container = new Composite(parent, SWT.NULL);
+		GridLayout layout = new GridLayout();
+		layout.numColumns = 3;
+		layout.verticalSpacing = 9;
+		container.setLayout(layout);	
+		
+		GridData gd = new GridData(GridData.FILL_HORIZONTAL);		
+		Label destinationLabel = new Label(container, SWT.NONE);
+		destinationLabel.setText("titulo");
+		
+		fileText = new Text(container, SWT.BORDER | SWT.SINGLE);
+		fileText.setEditable(false);
+		fileText.setLayoutData(gd);
+		
+		selectFileButton = new Button(container, SWT.PUSH);
+		selectFileButton.setText("Browse ...");
+		selectFileButton.addListener(SWT.Selection, new Listener() {
+			
+			@Override
+			public void handleEvent(Event event) {
+				fileDialog = new FileDialog(getShell(), SWT.SAVE);
+				fileDialog.setFilterNames(new String[] { "XML Files"});
+				fileDialog.setFilterExtensions(new String[] { "*.xml" });	
+				filePath = fileDialog.open();	
+				fileText.setText(filePath);
+				wizardChanged();
+			}
+		});  
+			
+		wizardChanged();
+		setControl(container);
 	}
 
-	@Override
-	public boolean performFinish() {
-		// TODO Auto-generated method stub
-		return false;
+	
+	protected void wizardChanged() {		
+		// The file path can't be empty
+		if (filePath.equals("")) {
+			updateStatus("Debes seleccionar un archivo");			
+		}
+		
+		else  
+			updateStatus(null);
 	}
+	
+	 public boolean finish() {
+		 boolean result = false;
+		 if (Controller.getInstance().isLogged()) {
+			 if (!filePath.equals(""))
+				 result = true;
+			 else
+				 updateStatus("Debes seleccionar un archivo");
+		 } else
+			 MessageDialog.openError(getShell(), "Error", "Debes estar logueado para realizar esta acción");
+		 return result;
+		 
+	 }
+	 
+	 protected void updateStatus(String message) {
+		setErrorMessage(message);
+		setPageComplete(message == null);		
+	}
+
+
+	public String getFilePath() {
+		return filePath;
+	}
+	 
+	
 
 }
