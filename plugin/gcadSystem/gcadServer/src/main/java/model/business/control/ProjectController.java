c1d74da438f04af2356241efb253327c67f51348
juanro.1987 <juanro.1987@b36858b7-eb87-cb19-e93d-fc6c9a9158f3>
2011-04-26 06:59:20 +0000

    - Server: se están poniendo mensajes de log en se servidor, usando un gestor de logs.

--- plugin/gcadSystem/gcadServer/src/main/java/model/business/control/ProjectController.java
+++ plugin/gcadSystem/gcadServer/src/main/java/model/business/control/ProjectController.java
@@ -11,16 +11,10 @@ import model.business.knowledge.Project;
  */
 public class ProjectController {
 
-	// TODO: traducir
-	// Método para registrar un nuevo beneficiario en el sistema
+	// Method used to create a new project and insert it into database
 	public static void createProject(Project project) throws SQLException {			
-		/* Consultamos si ya existe otro proyecto con los mismos datos
-		existe = PFProject.exists(project);
-		if(existe) {
-			throw new ExistingProject("Ya existe una persona en el sistema registrada con el NIF " + beneficiario.getNif() + "."); 
-		}*/
-		
-		// Añadimos el proyecto al sistema
+		// Check if have permission to perform the operation
+		SessionController.checkPermission(sessionId, new Operation(Groups.Project.name(), Subgroups.Project.name(), Operations.Add.name()));
 		DAOProject.insert(project);
 	}
 }
