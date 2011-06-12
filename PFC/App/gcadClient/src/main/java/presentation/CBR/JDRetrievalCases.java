cb30c23d6acc0afb799ac43d3b69f8e5ddf90f14
juanro.1987 <juanro.1987@b36858b7-eb87-cb19-e93d-fc6c9a9158f3>
2011-06-12 18:59:46 +0000

    - Client: creado el dialogo y el panel para mostrar informacion de un proyeto (usando reflexion)

---
+++ PFC/App/gcadClient/src/main/java/presentation/CBR/JDRetrievalCases.java
@@ -0,0 +1,126 @@
+package presentation.CBR;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+
+import javax.swing.JButton;
+
+import javax.swing.ActionMap;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.SwingUtilities;
+
+import model.business.knowledge.Project;
+
+import org.jdesktop.application.Action;
+import org.jdesktop.application.Application;
+
+/**
+* This code was edited or generated using CloudGarden's Jigloo
+* SWT/Swing GUI Builder, which is free for non-commercial
+* use. If Jigloo is being used commercially (ie, by a corporation,
+* company or business for any purpose whatever) then you
+* should purchase a license for each developer using Jigloo.
+* Please visit www.cloudgarden.com for details.
+* Use of Jigloo implies acceptance of these licensing terms.
+* A COMMERCIAL LICENSE HAS NOT BEEN PURCHASED FOR
+* THIS MACHINE, SO JIGLOO OR THIS CODE CANNOT BE USED
+* LEGALLY FOR ANY CORPORATE OR COMMERCIAL PURPOSE.
+*/
+public class JDRetrievalCases extends javax.swing.JDialog {
+	private panelCaseInformation currentPanel;
+	private JButton btnBackward;
+	private JButton btnForward;
+	private JLabel lblNumberCases;
+
+	/**
+	* Auto-generated main method to display this JDialog
+	*/		
+	
+	private List<Project> cases;
+	private int currentProject;
+	
+	public JDRetrievalCases(JFrame frame) {
+		super(frame);
+		cases = new ArrayList<Project> ();
+		currentProject = 1;
+		Project p1 = new Project("p1", "pa", new Date(), new Date(), 2000.1, 100, "aa", "C#", 20);
+		Project p2 = new Project("p2", "pb", new Date(), new Date(), 202575, 100, "aa", "Java", 20);
+		cases.add(p1);
+		cases.add(p2);
+		initGUI();
+		currentPanel.showData(cases.get(currentProject-1));
+	}
+	
+	private ActionMap getAppActionMap() {
+        return Application.getInstance().getContext().getActionMap(this);
+    }
+	
+	private void initGUI() {		
+		try {
+			{
+				getContentPane().setLayout(null);
+				{
+					currentPanel = new panelCaseInformation();
+					getContentPane().add(currentPanel);
+					currentPanel.setBounds(0, 0, 328, 410);
+				}
+				{
+					lblNumberCases = new JLabel();
+					getContentPane().add(lblNumberCases);
+					lblNumberCases.setBounds(117, 422, 79, 16);
+					lblNumberCases.setName("lblNumberCases");
+					lblNumberCases.setText("Case " + currentProject+"/"+cases.size());
+				}
+				{
+					btnForward = new JButton();
+					getContentPane().add(btnForward);
+					btnForward.setBounds(196, 419, 45, 23);
+					btnForward.setAction(getAppActionMap().get("Forward"));
+					btnForward.setName("btnForward");
+				}
+				{
+					btnBackward = new JButton();
+					getContentPane().add(btnBackward);
+					btnBackward.setBounds(65, 419, 43, 23);
+					btnBackward.setAction(getAppActionMap().get("Backward"));
+					btnBackward.setName("btnBackward");
+					btnBackward.setEnabled(false);
+				}
+			}
+			this.setSize(551, 542);
+			Application.getInstance().getContext().getResourceMap(getClass()).injectComponents(getContentPane());
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	@Action
+	public void Forward() {
+		if (currentProject < cases.size()) {
+			currentProject++;
+			btnBackward.setEnabled(true);
+			currentPanel.showData(cases.get(currentProject-1));
+			lblNumberCases.setText("Case " + currentProject+"/"+cases.size());
+		}
+		if(currentProject == cases.size()) {
+			btnForward.setEnabled(false);
+		}
+	}
+	
+	@Action
+	public void Backward() {
+		if (currentProject > 0) {
+			currentProject--;
+			btnForward.setEnabled(true);
+			currentPanel.showData(cases.get(currentProject-1));
+			lblNumberCases.setText("Case " + currentProject+"/"+cases.size());
+		}
+		if(currentProject == 1) {
+			btnBackward.setEnabled(false);
+		}
+	}
+}
