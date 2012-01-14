87119a6bccd020b1cda18a396db04ecc52daeca5
juanro.1987 <juanro.1987@b36858b7-eb87-cb19-e93d-fc6c9a9158f3>
2012-01-14 17:56:30 +0000

    - Aplicando revisiones

--- PFC/MemoriaModif/Codigo/marshal.java
+++ PFC/MemoriaModif/Codigo/marshal.java
@@ -1,7 +1,6 @@
 // Marshal domain class into XML file, using JAXB
 public static <E> ByteArrayOutputStream marshal(Class<E> className, Object obj) throws JAXBException {
 	JAXBContext jaxbContent = JAXBContext.newInstance(className);
-
 	Marshaller marshaller = jaxbContent.createMarshaller();
 	marshaller.setProperty(javax.xml.bind.Marshaller.JAXB_ENCODING, "UTF-8"); 
 	marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
